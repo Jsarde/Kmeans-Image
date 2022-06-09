538193a00ed73f10b36bf635736931005c2350a8
Jsarde <106827273+Jsarde@users.noreply.github.com>
2022-06-09 17:38:31 +0200

    Add files via upload

---
+++ kmeans_coins.py
@@ -0,0 +1,45 @@
+from PIL import Image, ImageOps
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+
+with Image.open('../Data/coins.jpg') as img:
+    border = (0, 0, 0, 20)
+    crop_image = ImageOps.crop(img, border)
+    gray_image = ImageOps.grayscale(crop_image)
+
+    #gray_image.show()
+    arr_img = np.array(gray_image)
+    # print(arr_img)
+    # print(arr_img.shape)
+
+    flatten_image = np.ndarray.flatten(arr_img, order='C')
+    # print(flatten_image)
+    flatten_image = np.expand_dims(flatten_image, axis=1)
+    # print(flatten_image)
+    ax = plt.subplot(2, 2, 1)
+    ax.imshow(crop_image)
+    ax.set_title('Original Coin')
+
+    for k in range(2, 4 + 1, 1):
+        model = KMeans(n_clusters=k)
+        model.fit(flatten_image)
+
+        res = model.labels_
+        #print(set(res)) -> visualizzo classi
+
+        # print(res)
+        # rint(res.shape)
+        img2 = np.reshape(res, newshape=arr_img.shape)
+
+        # print(results.shape)
+
+        # img2 = Image.fromarray(results)
+        # img2 = img2.convert('L')
+
+        ax = plt.subplot(2, 2, k)
+        ax.imshow(img2)
+        ax.set_title(f'Kmeans={k} Coins')
+
+    plt.show()
